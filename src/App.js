7bc349fab2ec2fdd02bd3ffa54bc31a87afeb8b6
Saurav Pratihar <pratihar.saurav@gmail.com>
2017-08-01 22:32:28 +0530

    uplaod frontEnd

---
+++ src/App.js
@@ -0,0 +1,77 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import './Login.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" /> 
+          <h2>Welcome to Hot OR Not</h2>
+
+
+        </div>
+        <Login />
+
+        <Register />
+      </div>
+    );
+  }
+}
+
+// Constant Login
+const Login = () =>{
+  return (
+    <div className='login'>
+      <form>
+        <div className="container">
+
+          <label />Email ID:
+          <input type="text" placeholder="Enter Email ID" name="email" required />
+
+          <label />Password:
+          <input type="password" placeholder="Enter Password" name="password" required />
+                
+          <input type="submit" value="Login" />
+            
+        </div>
+          
+      </form>
+    </div>
+  )
+}
+
+// Register
+const Register = () =>{
+  return (
+    <div className='login'>
+      <form>
+        <div className="container">
+
+          <label />Name:
+          <input type="text" placeholder="Enter Name" name="name" required />
+
+          <label />Email ID:
+          <input type="text" placeholder="Enter Email ID" name="email" required />
+
+          <label />Password:
+          <input type="password" placeholder="Enter Password" name="password" required />
+
+          <label />Re-Enter Password:
+          <input type="password" placeholder="Enter Password" name="password" required />
+                
+          <input type="submit" value="Register" />
+            
+        </div>
+          
+      </form>
+    </div>
+  )
+}
+
+
+
+
+export default App;
